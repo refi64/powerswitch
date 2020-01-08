7480bb3afbaa59c7c0806a728a89b1640a346d45
Ryan Gonzalez <rymg19@gmail.com>
2020-01-08 16:24:18 -0600

    systemd service sandboxing

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+set -ex
+
 install -Dm 755 powerswitch.py /usr/local/bin/powerswitch
 install -Dm 644 powerswitch.service /usr/local/lib/systemd/system/powerswitch.service
+install -Dm 644 powerswitch.rules /etc/polkit-1/rules.d/powerswitch.rules
